85f37e10131a055d5bd71a2111c3b1c20883a1a0
Roran60 <matusmatusovsky@gmail.com>
2024-06-18 19:52:42 +0200

    všetko premazané- čistý repozitár

--- public/js/aos-init.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  // ns-hugo:/home/roran/.cache/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/aos@v0.1.0/assets/mods/aos/js/options.ts
-  var options_default = {};
-
-  // <stdin>
-  AOS.init(options_default);
-})();
