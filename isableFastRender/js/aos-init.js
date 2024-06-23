9e4581174fbc2736578d4722f9d4222c377825f3
Roran600 <matusmatusovsky@gmail.com>
2024-06-23 22:06:05 +0200

    vymazané jazyky zh-hans a anglický, vymazané anglické menu support

---
+++ isableFastRender/js/aos-init.js
@@ -0,0 +1,7 @@
+(() => {
+  // ns-hugo:/home/roran/.cache/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/aos@v0.1.0/assets/mods/aos/js/options.ts
+  var options_default = {};
+
+  // <stdin>
+  AOS.init(options_default);
+})();
